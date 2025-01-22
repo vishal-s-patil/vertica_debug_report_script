a08f416cf5bfdb9e9fadaf9c4e8f61f45e72e9b5
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-01-22 14:23:16 +0530

    updated

--- app.py
+++ app.py
@@ -81,6 +81,14 @@ def replace_conditions(query, conditions_dict):
             placeholder = match.split(operator, 1)[1].strip()
             placeholder = placeholder.strip("'")
 
+            flag = 0
+            if placeholder.startswith("%") and placeholder.endswith("%"):
+                flag = 3
+            elif placeholder.startswith("%"):
+                flag = 1
+            elif placeholder.endswith("%"):
+                flag = 2
+
             print("cop", column_name, operator, placeholder)
             
             print('conditions_dict', conditions_dict)
@@ -90,7 +98,14 @@ def replace_conditions(query, conditions_dict):
                 if isinstance(value, int):
                     new_condition = f"AND {column_name} {operator} {value}"
                 else:
-                    new_condition = f"AND {column_name} {operator} '{value}'"
+                    if flag == 3:
+                        new_condition = f"AND {column_name} {operator} '%{value}%'"
+                    elif flag == 1:
+                        new_condition = f"AND {column_name} {operator} '%{value}'"
+                    elif flag == 2:
+                        new_condition = f"AND {column_name} {operator} '{value}%'"
+                    else:
+                        new_condition = f"AND {column_name} {operator} '{value}'"
                 
                 query = query.replace(f"{{{match}}}", new_condition)
     
