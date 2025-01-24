412b588b2fd495ddb079cba7a1d2e7562c576f95
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-01-24 15:33:09 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -409,7 +409,7 @@ if __name__ == "__main__":
     parser.add_argument("--pool_name", required=False, default=None, 
         help="Filter condition for queries with the 'pool_name' placeholder.")
     
-    parser.add_argument("--user_name", required=False, default='', 
+    parser.add_argument("--user_name", required=False, default=None, 
         help="Filter condition for queries with the 'user_name' placeholder.")
 
     parser.add_argument("--table_name", required=False, default=None, 
