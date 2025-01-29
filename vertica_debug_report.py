d07d281c5fe2f8d67255a965116bbe64acd9e12e
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-01-29 13:39:00 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -251,13 +251,13 @@ def analyse(query_name, query_result, query_description, column_headers, insight
                     if not is_result_printed:
                         is_result_printed = True
                     
-                    print(f"\n\nQuery Name: {query_name}")
-                    print("-" * len(f"Query Name: {query_name}"))
-                    print(f"Query Description: {query_description}")
-                    print("-" * len(f"Query Description: {query_description}"))
+                        print(f"\n\nQuery Name: {query_name}")
+                        print("-" * len(f"Query Name: {query_name}"))
+                        print(f"Query Description: {query_description}")
+                        print("-" * len(f"Query Description: {query_description}"))
 
-                    # if with_insights:
-                    print(tabulate(query_result, headers=column_headers, tablefmt='grid'))
+                        # if with_insights:
+                        print(tabulate(query_result, headers=column_headers, tablefmt='grid'))
                 
                 if item['threshold']['ok'] != 0 and ok_count > 0:
                     ok_count += wanr_count + fatal_count
