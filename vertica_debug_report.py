72de37ad20835be2a9c129afb780eaa9eb7d84a4
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-01-29 18:06:58 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -216,7 +216,7 @@ def get_error_messages_query():
     """
 
 
-def analyse(query, verbose, query_name, query_result, query_description, column_headers, insights_only, with_insights):
+def analyse(query, verbose, query_name, query_result, query_description, column_headers, insights_only, with_insights, duration):
     threshold_json_file_path = "thresholds.json"
     json_data = None
     with open(threshold_json_file_path) as json_file:
@@ -405,7 +405,7 @@ def execute_queries_from_json(json_file_path, filters, verbose, is_now, insights
                 
                 if processed_query_result:
                     if insights_only or with_insights:
-                        analyse(final_query, verbose, query_name, processed_query_result, query_description, column_headers, insights_only, filters["duration"], with_insights)
+                        analyse(final_query, verbose, query_name, processed_query_result, query_description, column_headers, insights_only, filters["duration"], with_insights, filters["duration"])
                     else:
                         print(f"\n\nQuery Name: {query_name}")
                         print("-" * len(f"Query Name: {query_name}"))
@@ -424,7 +424,7 @@ def execute_queries_from_json(json_file_path, filters, verbose, is_now, insights
                             print("-" * 15)
                         print("No records found")
                     else:
-                        analyse(final_query, verbose, query_name, processed_query_result, query_description, column_headers, insights_only, filters["duration"], with_insights)
+                        analyse(final_query, verbose, query_name, processed_query_result, query_description, column_headers, insights_only, with_insights, filters["duration"])
                             
         vertica_connection.close()
     except Exception as e:
