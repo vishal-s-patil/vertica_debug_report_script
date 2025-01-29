6942f50fb410b255e4d439a02f71f12663e2b30b
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-01-29 11:45:02 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -216,7 +216,7 @@ def get_error_messages_query():
     """
 
 
-def analyse(query_name, query_result, insights_only, with_insights):
+def analyse(query_name, query_result, column_headers, insights_only, with_insights):
     threshold_json_file_path = "thresholds.json"
     
     json_data = None
