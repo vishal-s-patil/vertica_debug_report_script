92ea776891b586e2aa2118bfcd3c25a4749d3db4
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-01-29 22:11:59 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -394,9 +394,9 @@ def replace_thresholds(query, query_name):
     for threshold in thresholds:
         if threshold['query_name'] == query_name:
             print(str(threshold['columns'][0]['threshold']['warn']))
-            query = query.replace('{ok_threshold}', str(threshold['columns'][0]['threshold']['ok']))
-            query = query.replace('{warn_threshold}', str(threshold['columns'][0]['threshold']['warn']))
-            query = query.replace('{fatal_threshold}', str(threshold['columns'][0]['threshold']['fatal']))
+            query = query.replace("{ok_threshold}", str(threshold['columns'][0]['threshold']['ok']))
+            query = query.replace("{warn_threshold}", str(threshold['columns'][0]['threshold']['warn']))
+            query = query.replace("{fatal_threshold}", str(threshold['columns'][0]['threshold']['fatal']))
     
     return query
 
