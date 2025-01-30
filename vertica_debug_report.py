4f333f22ac6795f312908eb8fb27c55a6fe0d7f4
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-01-30 14:50:53 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -298,7 +298,7 @@ def analyse(query, verbose, query_name, query_result, query_description, column_
                         print(tabulate(query_result, headers=column_headers, tablefmt='grid'))
                     for i, item in enumerate(query_result):
                         print('item', item[0])
-                        if item[0] == "WARN":
+                        if "WARN" in item[0]:
                             r = (str('\033[93m') + str(item[1]) + str('\033[0m'))
                             print(f"[WARN] {r} queries are running for more than 5 mins.")
                         else:
