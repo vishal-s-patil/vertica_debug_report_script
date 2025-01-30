eddc15e543ec3dbaf0719f3f317da6b3314922e4
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-01-30 15:03:21 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -229,6 +229,8 @@ def analyse(query, verbose, query_name, query_result, query_description, column_
     
     for threshold in thresholds:
         if threshold['query_name'] == query_name:
+            if query_name == "long_running_queries_raw":
+                return
             if query_name == "resource_pool_status":
                 if pool_name is None:
                     print(pool_name)
