8839f1640d40f679a0d17e0211c91b85f86c566f
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-01-30 14:17:38 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -469,7 +469,7 @@ def execute_queries_from_json(json_file_path, filters, verbose, is_now, insights
                     column_headers = [desc[0] for desc in vertica_connection.cursor().description]
                     processed_query_result = process_query_result_and_highlight_text(query_result, column_headers)
 
-                if query_result and len(query_result) > 0 and query_name == "long_running_queries":
+                if query_result and len(query_result) > 0 and (query_name == "long_running_queries" or query_name == "long_running_queries_raw"):
                     query_result = format_relativedelta(query_result, column_headers)
 
                 if query_result == -1:
