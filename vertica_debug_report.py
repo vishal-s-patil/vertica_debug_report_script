62699466400a3ca799333e22d011f93aa7cc402c
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-01-31 16:36:34 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -282,7 +282,7 @@ def analyse(query, verbose, query_name, query_result, query_description, column_
             query = replace_row_num_limit(query, 1000)
             query_result = execute_vertica_query(vertica_connection, query)
             query_result_show = process_query_result_and_highlight_text(query_result_show, column_headers)
-
+            query_result_show = query_result
             if query_result == -1:
                 print(query_name, ": column not found\n")
                 return
