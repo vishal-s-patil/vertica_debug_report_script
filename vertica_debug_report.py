8b8363f50d87504915ab60a40cdbfb370a421978
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-01-31 16:35:03 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -278,13 +278,11 @@ def analyse(query, verbose, query_name, query_result, query_description, column_
     query_result_show = None
     for threshold in thresholds:
         if with_insights:
+            print(query)
             query_result_show = execute_vertica_query(vertica_connection, query)
             query = replace_row_num_limit(query, 1000)
-            if query_name == "queue_status":
-                print(query)
             query_result = execute_vertica_query(vertica_connection, query)
             print('query_result', len(query_result))
-            
             print('query_result_show', len(query_result_show))
             query_result_show = process_query_result_and_highlight_text(query_result_show, column_headers)
 
