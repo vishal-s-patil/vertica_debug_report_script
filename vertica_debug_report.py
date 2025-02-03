2fb554d9a7bcd880b2b220e37a11c84c066b0d91
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-02-03 18:26:53 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -347,7 +347,6 @@ def analyse(query, verbose, query_name, query_result, query_description, column_
                     
                     return
             elif query_name == "long_running_queries":
-                print('entered...')
                 if (query_result is None or len(query_result) == 0) and (issue_level == 'ok' or issue_level is None):
                     print("[OK] No long running queries.")
                 elif len(query_result) == 1:
