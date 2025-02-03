30cf0c1632fa393fb9e45d618501cddd0ec6bd1a
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-02-03 23:11:23 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -569,7 +569,6 @@ def format_relativedelta(query_result, column_headers, column_name="running_time
 
 
 def execute_queries_from_json(json_file_path, filters, verbose, is_now, insights_only, with_insights, queries_to_execute=None):
-    print('reached...')
     try:
         vertica_connection = get_vertica_connection()
         if not vertica_connection:
@@ -600,6 +599,7 @@ def execute_queries_from_json(json_file_path, filters, verbose, is_now, insights
                     continue
             
                 if query_name == "performance_buckets" and filters['user_name'] is None:
+                    print('Please provide a user name to use performance_buckets')
                     continue
 
                 if query_past == "":
