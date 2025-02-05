ae8316f24c0e4e88f2099ab7a1195c41f1ac804f
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-02-05 16:40:59 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -465,11 +465,9 @@ def analyse(query, verbose, query_name, query_result, query_description, column_
             for item in threshold['columns']:
                 if item['columns_name'] == "deleted_row_cnt":
                     if query_result_show:
-                        query_result_show = handle_deleted_row_count(query_result, query_result_show, item, with_insights, threshold, column_headers)
-                        print(query_result_show)
+                        query_result = handle_deleted_row_count(query_result, query_result_show, item, with_insights, threshold, column_headers)
                     else:
                         query_result = handle_deleted_row_count(query_result, query_result_show, item, with_insights, threshold, column_headers)
-                        print(query_result)
 
                 if query_result == None or len(query_result) == 0:
                     if item['default_message'] is not "":
