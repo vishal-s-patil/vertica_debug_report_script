8c363133921cc5a065d9258df83da0f7eeb299be
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-02-05 12:16:26 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -483,10 +483,10 @@ def analyse(query, verbose, query_name, query_result, query_description, column_
                             print(f"\n\nQuery Name: {query_name}")
                             print("-" * len(f"Query Name: {query_name}"))
                             if query_result_show is not None:
-                                query_result_show = colour_values(query_result_show, threshold['query_name']['columns'], column_headers)
+                                # query_result_show = colour_values(query_result_show, threshold['query_name']['columns'], column_headers)
                                 print(tabulate(query_result_show, headers=column_headers, tablefmt='grid', floatfmt=".2f"))
                             else:
-                                query_result = colour_values(query_result, threshold['query_name']['columns'], column_headers)
+                                # query_result = colour_values(query_result, threshold['query_name']['columns'], column_headers)
                                 print(tabulate(query_result, headers=column_headers, tablefmt='grid', floatfmt=".2f"))
                 
                 flag = True
