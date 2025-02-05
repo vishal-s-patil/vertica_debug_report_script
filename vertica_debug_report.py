2b496ba95f64fddd7db3739d7bc8a1b0fd7c5e08
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-02-05 18:59:09 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -499,7 +499,6 @@ def analyse(query, verbose, query_name, query_result, query_description, column_
                         column_to_compare_index = column_headers.index("total_row_cnt")
                         if item['unique_column'] == "":
                             for row in query_result:
-                                print(row[index])
                                 if row[index] > int(row[column_to_compare_index])*(fatal_threshold/100):
                                     fatal_count+=1
                                 elif row[index] > int(row[column_to_compare_index])*(warn_threshold/100):
