ba57542781b692ef2c8b9e9c49c2990e41b80198
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-02-05 16:10:05 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -264,9 +264,9 @@ def colour_values_deleted_row_count(query_result, item, with_insights, threshold
         ansi_escape = re.compile(r'\x1b\[[0-9;]*m')
         row[column_to_colour_index] = ansi_escape.sub('', row[column_to_colour_index])
         
-        if int(row[column_to_colour_index]) > int(row[column_to_compare_index])*0.5:
+        if int(row[column_to_colour_index]) > int(row[column_to_compare_index])*0.99:
             row[column_to_colour_index] = str('\033[91m') + str(row[column_to_colour_index]) + str('\033[0m')
-        elif int(row[column_to_colour_index]) > row[column_to_compare_index]*0.1:
+        elif int(row[column_to_colour_index]) > row[column_to_compare_index]*0.99:
             row[column_to_colour_index] = str('\033[93m') + str(row[column_to_colour_index]) + str('\033[0m')
         else:
             row[column_to_colour_index] = str('\033[92m') + str(row[column_to_colour_index]) + str('\033[0m')
