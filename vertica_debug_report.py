ad43e210a09a0b92ef2bdc2e5d36ecccb3fd1c18
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-02-06 22:21:25 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -749,7 +749,7 @@ def execute_queries_from_json(json_file_path, filters, verbose, is_now, insights
 def execute_query_breakdown(args):
     q = query_breakdown(args.client_breakdown, args.granularity, args.query_pattern, args.query_breakdown_chars, args.case_sensitive, args.num_items, args.duration_hours, args.issue_time)
     vertica_connection = vertica.get_vertica_connection()
-    q_res = vertica.execute_vertica_query(q)
+    q_res = vertica.execute_vertica_query(vertica_connection, q)
 
     if not q_res or len(q_res) == 0:
         print(f"\n\nQuery Name: {query_name}")
