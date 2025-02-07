5a4836e267c978ed9ef134296ea4d458a58cbe28
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-02-07 11:12:40 +0530

    updated

--- query_breakdown.py
+++ query_breakdown.py
@@ -64,10 +64,10 @@ def query_breakdown(client_breakdown, granularity, query_pattern, query_breakdow
 
     elif granularity and client_breakdown and query_breakdown_chars:
         # granularity and client_breakdown and query_breakdown_chars
-        # granularity_dimension = granularity_dimension.replace('{duration}', f"{granularity}")
+        granularity_dimension = granularity_dimension.replace('{duration}', f"{granularity}")
         query_dimension = query_dimension.replace('{query_breakdown_chars}', f"{query_breakdown_chars}")
         
-        d["dimension_replacements"] = query_dimension + ', ' + aggregations
+        d["dimension_replacements"] = granularity_dimension + ', ' + client_breakdown_dimension + ', ' + query_dimension
         d["groupby_replacements"] = "1, 2, 3"
         
 
@@ -76,12 +76,12 @@ def query_breakdown(client_breakdown, granularity, query_pattern, query_breakdow
 
     else:
         # granularity='hour' 
-        query_breakdown_chars=20
+        # query_breakdown_chars=20
         # granularity_dimension = granularity_dimension.replace('{duration}', f"{granularity}")
-        # query_dimension = query_dimension.replace('{query_breakdown_chars}', f"{query_breakdown_chars}")
+        query_dimension = query_dimension.replace('{query_breakdown_chars}', f"{query_breakdown_chars}")
 
-        d["dimension_replacements"] = granularity_dimension + ", " + client_breakdown_dimension + ", " + query_dimension
-        d["groupby_replacements"] = "1, 2, 3"
+        d["dimension_replacements"] = query_dimension + ', ' + aggregations
+        d["groupby_replacements"] = "1"
 
         q = replace_conditions(body, d)
 
