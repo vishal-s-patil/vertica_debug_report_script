8cd470cd71e35a38d9d52ad4f74ef86cdd135bfb
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-02-07 14:13:12 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -790,29 +790,3 @@ def execute_query_breakdown(args, is_now, verbose):
 #     insights_json = {}
 
 #     execute_queries_from_json(insights_json, json_file_path, filters, filters['verbose'], is_now, insights_only, with_insights, queries_to_execute)
-
-    
-
-app = Flask(__name__)
-
-@app.route('/globalrefresh', methods=['GET'])
-def greet():
-    args = get_args()
-    filters, is_now, insights_only, with_insights, json_file_path, queries_to_execute = pargse_args()
-
-    insights_json = {}
-
-    execute_queries_from_json(insights_json, json_file_path, filters, filters['verbose'], is_now, insights_only, with_insights, queries_to_execute)
-
-    # name = request.args.get('name', 'Guest')
-    # age = request.args.get('age', 'unknown')
-    
-    # response = {
-    #     'message': f'Hello, {name}!',
-    #     'age': age
-    # }
-    
-    return jsonify(insights_json)
-
-
-app.run(host='0.0.0.0', port=5500)
