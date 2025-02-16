33e902b8cd98f456af0b0e2fde638c22711d5d87
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-02-16 16:12:21 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -737,6 +737,7 @@ if __name__ == "__main__":
             if sys.argv[2] == "nodes":
                 print(tabulate(nodes, headers=['node', 'ip', 'subcluster'], tablefmt='grid', floatfmt=".2f"))   
             elif sys.argv[2] == "subclusters":
+                print(set(subclusters))
                 print(tabulate(list(set(subclusters)), headers=['subcluster'], tablefmt='grid', floatfmt=".2f"))   
             else:
                 print('else')
