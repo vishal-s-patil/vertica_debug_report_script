72ac37264bfbfc09f949f40227033a319849f4af
vishal-s-patil <vishalspolicepatil55@gmail.com>
2025-02-18 15:57:53 +0530

    updated

--- vertica_debug_report.py
+++ vertica_debug_report.py
@@ -656,6 +656,7 @@ def execute_queries_from_json(insights_json, json_file_path, filters, verbose, i
                     print(f"Error reading {threshold_json_file_path}")
                     exit()
 
+                print('reached.......')
                 if processed_query_result:
                     if insights_only or with_insights:
                         analyse(qid, insights_json, final_query, verbose, query_name, processed_query_result, query_description, column_headers, insights_only, with_insights, filters["duration"], filters["pool_name"], filters["issue_level"], is_now, filters['user_name'],filters['subcluster_name'], filters['issue_time'], vertica_connection, filters) 
@@ -683,7 +684,6 @@ def execute_queries_from_json(insights_json, json_file_path, filters, verbose, i
                     else:
                         analyse(qid, insights_json, final_query, verbose, query_name, processed_query_result, query_description, column_headers, insights_only, with_insights, filters["duration"], filters["pool_name"], filters["issue_level"], is_now, filters['user_name'],filters['subcluster_name'], filters['issue_time'], vertica_connection, filters)
         vertica_connection.close()
-        print('reached.......')
     except Exception as e:
         print(f"Error while processing the CSV file or executing queries: {e}")
     
